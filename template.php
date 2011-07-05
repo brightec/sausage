4c35c93fc843545af2d0c746c6e249d6952013b1
Erik Erskine <erik@brightec.co.uk>
2011-07-05 08:39:07 +0100

    add panels or not-panels class to body element

--- template.php
+++ template.php
@@ -1,6 +1,14 @@
 <?php
 
 
+function sausage_preprocess_html(&$variables) {
+  // add a panels or not-panels class to the body
+  if (module_exists('panels') && function_exists('panels_get_current_page_display')) {
+    $variables['classes_array'][] = panels_get_current_page_display() ? 'panels' : 'not-panels';
+  }
+}
+
+
 function sausage_preprocess_page(&$variables) {
 
   // if there is a page_title panel present, then don't show
